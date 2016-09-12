ce641e298058229f8c69fa06d7c1a9b4d2220e51
Mathieu Civel <mathieu@civel.eu>
2016-09-12 17:45:33 +0200

    set base layout style

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,41 +1,73 @@
 <template>
-  <div :class="theme">
-    <h1>Speculos</h1>
-    <menu-left></menu-left>
-    <router-view></router-view>
+  <div class="app-structure" :class="theme">
+
+    <menu-top></menu-top>
+
+    <!-- Semantic UI Sidebar -->
+    <div class="sidebar-structure pushable">
+      <div class="ui left visible vertical labeled icon menu thin sidebar">
+        <router-link to="overview">
+          <a class="item"><i class="line chart icon"></i>Overview</a>
+        </router-link>
+        <router-link to="bots">
+          <a class="item"><i class="game icon"></i>Bots</a>
+        </router-link>
+        <router-link to="instances">
+          <a class="item"><i class="plug icon"></i>Instances</a>
+        </router-link>
+        <router-link to="markets">
+          <a class="item"><i class="bitcoin icon"></i>Markets</a>
+        </router-link>
+      </div>
+      <div class="pusher">
+        <!-- Router will mount components here -->
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import MenuLeft from './MenuLeft.vue'
-import { mapGetters } from 'vuex'
+  import $ from 'jquery'
+  import sidebar from 'semantic-ui-less/definitions/modules/sidebar.js'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name : 'App',
-  computed: {
-    ...mapGetters({
-      theme : 'theme'
-    })
-  },
-  data () {
-    return {
+  import MenuTop from './MenuTop.vue'
+
+  export default {
+    name : 'App',
+    computed: {
+      ...mapGetters({
+        theme : 'theme'
+      })
+    },
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      $('.sidebar-structure .ui.sidebar').sidebar({
+        context: $('.sidebar-structure')
+      })
+    },
+    components : {
+      'menu-top' : MenuTop
     }
-  },
-  mounted () {
-
-  },
-  components : {
-    'menu-left' : MenuLeft
   }
-}
 </script>
 
 <style lang="less">
-@import "../styles/semantic.less";
-@textColor : #333;
+  @import "../styles/semantic.less";
 
-body {
-  color : @textColor;
-  font-family: Helvetica, sans-serif;
-}
+  div.app-structure {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+  }
+
+  div.ui.sidebar {
+    width: 141px !important;
+    border-top: none;
+  }
 </style>
---
+++ src/components/MenuTop.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui top attached menu">
+    <a class="item">
+      <i class="sidebar icon"></i>
+    </a>
+    <div class="header item">
+      Speculos
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'MenuTop',
+    data() {
+      return {
+      };
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {},
+    components: {}
+  };
+</script>
+
+<style scoped>
+  .ui.top.attached.menu {
+    border-top: none;
+  }
+</style>
