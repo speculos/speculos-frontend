3a829e28425615fbc1b02efd2f49ef1db1b12182
Mathieu Civel <mathieu.civel@gmail.com>
2016-10-24 14:38:52 +0200

    add altcoin fonticon

---
+++ currencies.js
@@ -0,0 +1,363 @@
+window.config = window.config || {}
+window.config.currencies  = {
+  "1CR" : {
+    name : "1CRedit",
+    logo : "circle thin"
+  },
+  "AMP" : {
+    name : "Synereo AMP",
+    logo : "cc AMP"
+  },
+  "ARDR" : {
+    name : "Ardor",
+    logo : "cc ARDR"
+  },
+  "BBR" : {
+    name : "Boolberry",
+    logo : "circle thin"
+  },
+  "BCN" : {
+    name : "Bytecoin",
+    logo : "cc BCN"
+  },
+  "BCY" : {
+    name : "BitCrystals",
+    logo : "circle thin"
+  },
+  "BELA" : {
+    name : "BellaCoin",
+    logo : "circle thin"
+  },
+  "BITCNY" : {
+    name : "BitCNY",
+    logo : "circle thin"
+  },
+  "BITS" : {
+    name : "Bitstar",
+    logo : "circle thin"
+  },
+  "BLK" : {
+    name : "BlackCoin",
+    logo : "circle thin"
+  },
+  "BLOCK" : {
+    name : "Blocknet",
+    logo : "circle thin"
+  },
+  "BTCD" : {
+    name : "BitcoinDark",
+    logo : "cc BTCD"
+  },
+  "BTC" : {
+    name : "Bitcoin",
+    logo : "cc BTC"
+  },
+  "BTM" : {
+    name : "Bitmark",
+    logo : "circle thin"
+  },
+  "BTS" : {
+    name : "BitShares",
+    logo : "cc BTS"
+  },
+  "BURST" : {
+    name : "Burst",
+    logo : "circle thin"
+  },
+  "C2" : {
+    name : "Coin2.0",
+    logo : "circle thin"
+  },
+  "CGA" : {
+    name : "Cryptographic Anomaly",
+    logo : "circle thin"
+  },
+  "CLAM" : {
+    name : "CLAMS",
+    logo : "cc CLAM"
+  },
+  "CURE" : {
+    name : "Curecoin",
+    logo : "circle thin"
+  },
+  "DASH" : {
+    name : "Dash",
+    logo : "cc DASH"
+  },
+  "DCR" : {
+    name : "Decred",
+    logo : "cc DCR"
+  },
+  "DGB" : {
+    name : "DigiByte",
+    logo : "cc DGB"
+  },
+  "DIEM" : {
+    name : "Diem",
+    logo : "circle thin"
+  },
+  "DOGE" : {
+    name : "Dogecoin",
+    logo : "cc DOGE"
+  },
+  "EMC2" : {
+    name : "Einsteinium",
+    logo : "circle thin"
+  },
+  "ETC" : {
+    name : "Ethereum Classic",
+    logo : "cc ETC"
+  },
+  "ETH" : {
+    name : "Ethereum",
+    logo : "cc ETH"
+  },
+  "EXP" : {
+    name : "Expanse",
+    logo : "circle thin"
+  },
+  "FCT" : {
+    name : "Factom",
+    logo : "cc FCT"
+  },
+  "FLDC" : {
+    name : "FoldingCoin",
+    logo : "circle thin"
+  },
+  "FLO" : {
+    name : "Florincoin",
+    logo : "circle thin"
+  },
+  "GAME" : {
+    name : "GameCredits",
+    logo : "circle thin"
+  },
+  "GEO" : {
+    name : "GeoCoin",
+    logo : "circle thin"
+  },
+  "GRC" : {
+    name : "Gridcoin Research",
+    logo : "cc GRC"
+  },
+  "HUC" : {
+    name : "Huntercoin",
+    logo : "circle thin"
+  },
+  "HZ" : {
+    name : "Horizon",
+    logo : "circle thin"
+  },
+  "IOC" : {
+    name : "IO Digital Currency",
+    logo : "cc IOC"
+  },
+  "LBC" : {
+    name : "LBRY Credits",
+    logo : "cc LBC"
+  },
+  "LSK" : {
+    name : "Lisk",
+    logo : "circle thin"
+  },
+  "LTBC" : {
+    name : "LTBCoin",
+    logo : "circle thin"
+  },
+  "LTC" : {
+    name : "Litecoin",
+    logo : "cc LTC"
+  },
+  "MAID" : {
+    name : "MaidSafeCoin",
+    logo : "cc MAID"
+  },
+  "MMNXT" : {
+    name : "MMNXT",
+    logo : "circle thin"
+  },
+  "MYR" : {
+    name : "Myriadcoin",
+    logo : "circle thin"
+  },
+  "NAUT" : {
+    name : "Nautiluscoin",
+    logo : "circle thin"
+  },
+  "NAV" : {
+    name : "NAVCoin",
+    logo : "circle thin"
+  },
+  "NBT" : {
+    name : "NuBits",
+    logo : "cc NBT"
+  },
+  "NEOS" : {
+    name : "Neoscoin",
+    logo : "cc NEOS"
+  },
+  "NMC" : {
+    name : "Namecoin",
+    logo : "cc NMC"
+  },
+  "NOBL" : {
+    name : "NobleCoin",
+    logo : "circle thin"
+  },
+  "NOTE" : {
+    name : "DNotes",
+    logo : "cc NOTE"
+  },
+  "NSR" : {
+    name : "NuShares",
+    logo : "circle thin"
+  },
+  "NXT" : {
+    name : "NXT",
+    logo : "cc NXT"
+  },
+  "OMNI" : {
+    name : "Omni",
+    logo : "cc OMNI"
+  },
+  "PINK" : {
+    name : "Pinkcoin",
+    logo : "cc PINK"
+  },
+  "POT" : {
+    name : "PotCoin",
+    logo : "cc POT"
+  },
+  "PPC" : {
+    name : "Peercoin",
+    logo : "cc PPC"
+  },
+  "QBK" : {
+    name : "Qibuck",
+    logo : "circle thin"
+  },
+  "QORA" : {
+    name : "Qora",
+    logo : "circle thin"
+  },
+  "QTL" : {
+    name : "Quatloo",
+    logo : "circle thin"
+  },
+  "RADS" : {
+    name : "Radium",
+    logo : "circle thin"
+  },
+  "RBY" : {
+    name : "Rubycoin",
+    logo : "cc RBY"
+  },
+  "RDD" : {
+    name : "Reddcoin",
+    logo : "cc RDD"
+  },
+  "REP" : {
+    name : "Augur",
+    logo : "circle thin"
+  },
+  "RIC" : {
+    name : "Riecoin",
+    logo : "circle thin"
+  },
+  "SBD" : {
+    name : "Steem Dollars",
+    logo : "circle thin"
+  },
+  "SC" : {
+    name : "Siacoin",
+    logo : "circle thin"
+  },
+  "SDC" : {
+    name : "Shadow",
+    logo : "cc SDC"
+  },
+  "SJCX" : {
+    name : "Storjcoin X",
+    logo : "cc SJCX"
+  },
+  "STEEM" : {
+    name : "STEEM",
+    logo : "cc STEEM"
+  },
+  "STR" : {
+    name : "Stellar",
+    logo : "cc STR"
+  },
+  "SYNC" : {
+    name : "Sync",
+    logo : "cc SYNC"
+  },
+  "SYS" : {
+    name : "Syscoin",
+    logo : "cc SYS"
+  },
+  "UNITY" : {
+    name : "SuperNET",
+    logo : "cc UNITY"
+  },
+  "VIA" : {
+    name : "Viacoin",
+    logo : "circle thin"
+  },
+  "VOX" : {
+    name : "Voxels",
+    logo : "circle thin"
+  },
+  "VRC" : {
+    name : "VeriCoin",
+    logo : "circle thin"
+  },
+  "VTC" : {
+    name : "Vertcoin",
+    logo : "cc VTC"
+  },
+  "XBC" : {
+    name : "BitcoinPlus",
+    logo : "circle thin"
+  },
+  "XCN" : {
+    name : "Cryptonite",
+    logo : "circle thin"
+  },
+  "XCP" : {
+    name : "Counterparty",
+    logo : "circle thin"
+  },
+  "XDN" : {
+    name : "DigitalNote",
+    logo : "circle thin"
+  },
+  "XEM" : {
+    name : "NEM",
+    logo : "cc XEM"
+  },
+  "XMG" : {
+    name : "Magi",
+    logo : "circle thin"
+  },
+  "XMR" : {
+    name : "Monero",
+    logo : "cc XMR"
+  },
+  "XPM" : {
+    name : "Primecoin",
+    logo : "cc XPM"
+  },
+  "XRP" : {
+    name : "Ripple",
+    logo : "circle thin"
+  },
+  "XST" : {
+    name : "StealthCoin",
+    logo : "circle thin"
+  },
+  "XVC" : {
+    name : "Vcash",
+    logo : "circle thin"
+  }
+}
