7d652afd5faca7528122faf1202045224317e187
Mathieu Civel <mathieu.civel@gmail.com>
2016-10-27 17:01:26 +0200

    Added unit test framework (mocha + unexpected)

---
+++ tests/tradeStore.js
@@ -0,0 +1,10 @@
+var expect = require('unexpected')
+var tradeStore = require('../src/scripts/data/tradeStore.js')
+
+
+describe('tradeStore', function() {
+  it('should exist', function() {
+    expect(tradeStore, 'to be defined')
+  })
+
+})
